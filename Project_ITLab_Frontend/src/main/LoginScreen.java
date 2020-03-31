642ebfe7a02c009695f456c4c4bfffc4b69e7c0a
Dzhem <dzhem.aptula@gmail.com>
2020-03-31 15:31:39 +0200

    Some css, playing around with design and fixing stuff...

--- Project_ITLab_Frontend/src/main/LoginScreen.java
+++ Project_ITLab_Frontend/src/main/LoginScreen.java
@@ -1,15 +1,9 @@
 package main;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Tooltip;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 
 //  indien verkeerde login
--- Project_ITLab_Frontend/src/main/StartUp.java
+++ Project_ITLab_Frontend/src/main/StartUp.java
@@ -17,16 +17,15 @@ public class StartUp extends Application
     @Override
     public void start(Stage primaryStage)
     {
-        //DomeinController dc = new DomeinController();
+//        DomeinController dc = new DomeinController();
         MainScreen mainScreen = new MainScreen();
         Scene scene = new Scene(mainScreen, 700, 400);
 //        Font.loadFont(StartUp.class.getResource("Retro.ttf").toExternalForm(), 10);
 //        Font.loadFont(StartUp.class.getResource("SFAlienEncounters.ttf").toExternalForm(), 10);
-//        String uri = Paths.get("src/gui/Viper.css").toUri().toString();
-//        scene.getStylesheets().add(uri);
+        String uri = Paths.get("src/styles/style.css").toUri().toString();
+        scene.getStylesheets().add(uri);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Mastermind");
-        primaryStage.setFullScreen(true);
         primaryStage.show();
     }
 }
